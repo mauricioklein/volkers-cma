868d52847f0414b19711b0e751e834198ef73918
Mauricio Klein <mauricio.klein@fromatob.com>
2018-01-03 16:06:24 +0100

    ApplicationController refactoring
    - Adds extra general purpose rescues
    - Current user fetch is now centralized in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid_error
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_error
+  rescue_from CustomErrors::Unauthenticated, with: :unauthenticated_error
   rescue_from CustomErrors::Unauthorized, with: :unauthorized_error
 
 private
+
+  def current_user
+    @current_user = AuthenticationService.login_by_token(token)
+  end
+
+  def token
+    request.env['TOKEN']
+  end
 
   def record_invalid_error(error)
     render status: :unprocessable_entity, json: { errors: error }
@@ -13,11 +22,11 @@ private
     render status: :not_found, json: { errors: error }
   end
 
-  def unauthorized_error(error)
-    render status: :unauthorized
+  def unauthenticated_error(error)
+    render status: :forbidden
   end
 
-  def token
-    request.env['TOKEN']
+  def unauthorized_error(error)
+    render status: :unauthorized
   end
 end
