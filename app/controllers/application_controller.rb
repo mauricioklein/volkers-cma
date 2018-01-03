151270d2b67d8767a2c74debcc5881e668b7aec5
Mauricio Klein <mauricio.klein@fromatob.com>
2018-01-03 11:28:51 +0100

    Add rescue for Unauthorized errors on controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid_error
+  rescue_from CustomErrors::Unauthorized, with: :unauthorized_error
 
 private
 
   def record_invalid_error(error)
     render status: :unprocessable_entity, json: { errors: error }
+  end
+
+  def unauthorized_error(error)
+    render status: :unauthorized
   end
 end
