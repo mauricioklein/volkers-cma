bb82f0b9b1db49d5a77b94884cb60d7f13becd05
Mauricio Klein <mauricio.klein@fromatob.com>
2018-01-03 13:10:53 +0100

    Add ApplicationController treatment to record not found
    Also, the token extraction logic was moved to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid_error
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_error
   rescue_from CustomErrors::Unauthorized, with: :unauthorized_error
 
 private
@@ -8,7 +9,15 @@ private
     render status: :unprocessable_entity, json: { errors: error }
   end
 
+  def record_not_found_error(error)
+    render status: :not_found, json: { errors: error }
+  end
+
   def unauthorized_error(error)
     render status: :unauthorized
+  end
+
+  def token
+    request.env['TOKEN']
   end
 end
