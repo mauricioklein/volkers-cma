4f56cd2fdeb2cad6ff9a9143e9430ba26f13451e
Mauricio Klein <mauricio.klein@fromatob.com>
2018-01-03 16:03:44 +0100

    Add extra CustomErrors to system

--- app/errors/custom_errors.rb
+++ app/errors/custom_errors.rb
@@ -1,3 +1,11 @@
 module CustomErrors
+  # Authentication errors
+  class UserNotFound < StandardError; end
+  class Unauthenticated < StandardError; end
+
+  # Access errors
   class Unauthorized < StandardError; end
+
+  # Contract errors
+  class ContractInactive < StandardError; end
 end
